a0ff4d07ff8efe04586bb76cebc7f514661f4277
SajjanKarn <karnaa787@gmail.com>
2023-04-27 08:51:14 +0545

    successfully integrated contentful api in subjects screen

--- src/screens/SubjectScreen.js
+++ src/screens/SubjectScreen.js
@@ -7,53 +7,35 @@ import AppText from "../components/AppText";
 import SubjectCard from "../components/SubjectCard";
 import Underline from "../components/Underline";
 import FadeInView from "../components/FadeInView";
+import Loader from "../components/Loader";
+
+import { gql, useQuery } from "@apollo/client";
+
+const QUERY_COLLECTION = gql`
+  {
+    subjectsCollection {
+      items {
+        title
+        totalChapters
+        subjectThumbnail {
+          url
+        }
+      }
+    }
+  }
+`;
 
 export default function SubjectScreen() {
   const navigation = useNavigation();
-  const [subjects, setSubjects] = useState([
-    {
-      id: 1,
-      title: "Physics",
-      imageUrl:
-        "https://www.vedantu.com/seo/content-images/bc692335-2c03-4559-9cf0-dff766298eef.jpg",
-      totalChapter: 10,
-    },
-    {
-      id: 2,
-      title: "Chemistry",
-      imageUrl:
-        "https://council.science/wp-content/uploads/2017/04/IUPAC-feature-image.jpg",
-      totalChapter: 15,
-    },
-    {
-      id: 3,
-      title: "Mathematics",
-      imageUrl:
-        "https://www.stoodnt.com/blog/wp-content/uploads/2021/10/branches_of_mathematics.jpg",
-      totalChapter: 7,
-    },
-    {
-      id: 4,
-      title: "Biology",
-      imageUrl:
-        "https://images.shiksha.com/mediadata/images/articles/1538985491phpKctLgx.jpeg",
-      totalChapter: 12,
-    },
-    {
-      id: 5,
-      title: "Computer Science",
-      imageUrl:
-        "https://images.pexels.com/photos/276452/pexels-photo-276452.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      totalChapter: 8,
-    },
-    {
-      id: 6,
-      title: "English",
-      imageUrl:
-        "https://images.pexels.com/photos/256417/pexels-photo-256417.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      totalChapter: 10,
-    },
-  ]);
+  const { data, loading } = useQuery(QUERY_COLLECTION);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  console.log(data.subjectsCollection.items);
+
+  const [subjects, setSubjects] = useState(data.subjectsCollection.items);
   return (
     <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
       <View style={styles.contentContainer}>
@@ -63,13 +45,13 @@ export default function SubjectScreen() {
         <Underline />
 
         <View style={styles.cardContainer}>
-          {subjects.map((topic) => (
-            <FadeInView key={topic.id} style={styles.fadeStyle}>
+          {subjects.map((subject) => (
+            <FadeInView key={subject.title} style={styles.fadeStyle}>
               <SubjectCard
-                key={topic.id}
-                title={topic.title}
-                imageURL={topic.imageUrl}
-                subHeading={`${topic.totalChapter} Chapters`}
+                key={subject.title}
+                title={subject.title}
+                imageURL={subject.subjectThumbnail.url}
+                subHeading={`${subject.totalChapters} Chapters`}
                 onPress={() => navigation.navigate("SubjectTopicsScreen")}
               />
             </FadeInView>
